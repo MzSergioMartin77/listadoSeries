31ad741cde1d3f8882b341fea8494a97aa82803e
Sergio <26283302+MzSergioMartin77@users.noreply.github.com>
2020-06-09 11:03:43 +0200

    Componentes generados

--- front/listado-angular/src/app/app.module.ts
+++ front/listado-angular/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AddSerieComponent } from './add-serie/add-serie.component';
+import { UpdateSerieComponent } from './update-serie/update-serie.component';
+import { ListSeriesComponent } from './list-series/list-series.component';
+import { InfoSerieComponent } from './info-serie/info-serie.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AddSerieComponent,
+    UpdateSerieComponent,
+    ListSeriesComponent,
+    InfoSerieComponent
   ],
   imports: [
     BrowserModule,
---
+++ front/listado-angular/src/app/update-serie/update-serie.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-update-serie',
+  templateUrl: './update-serie.component.html',
+  styleUrls: ['./update-serie.component.scss']
+})
+export class UpdateSerieComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
