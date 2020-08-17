4a4da59399b3365e30fc75ec107951a16c3b3371
Sergio <26283302+MzSergioMartin77@users.noreply.github.com>
2020-08-17 19:26:15 +0200

    añadir CORS para conectar el front con el back

--- back/app.js
+++ back/app.js
@@ -6,12 +6,30 @@ const express = require('express');
 const bodyParse = require('body-parser');
 
 const serie_routes = require('./routes/serieRoutes');
+const cors = require('cors');
 
-const app = express();
+//const config = require('./config');
+
+const config = {
+    application: {
+        cors: {
+            server: [
+                {
+                    origin: "localhost:4200", //servidor que deseas que consuma o (*) en caso que sea acceso libre
+                    credentials: true
+                }
+            ]
+        }
+    }
+}
+
+var app = express();
 
 app.use(bodyParse.urlencoded({extended:false}));
 app.use(bodyParse.json()); //para pasar lo que se pase a json
-
+app.use(cors(
+    config.application.cors.server
+  ));
 //rutas
 app.use('/', serie_routes);
 
